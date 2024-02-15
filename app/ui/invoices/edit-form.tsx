193064d689422ff4ff9045d484d1f69acb969e8d
= <=>
2024-02-14 21:08:01 -0300

    feat: class 12 - mutate data

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { updateInvoice } from "@/app/lib/actions";
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
   CheckIcon,
@@ -10,6 +11,8 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 
+
+
 export default function EditInvoiceForm({
   invoice,
   customers,
@@ -17,8 +20,11 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id); 
+
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
